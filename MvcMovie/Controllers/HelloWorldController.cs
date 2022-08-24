2f75726edbccd05d5c65567c60f477611201ca02
avelicoglo <avelicoglo@GEORGE-PC>
2022-08-24 11:23:38 +0300

    Добавьте файлы проекта.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        //GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+            //return "Sapec, hello world. This is my deffaault action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        // ruquires using Sysytem.Text.Encodings.Web;
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = $"Hello {name}";
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"hi, {name}, NumTimes is: {ID}");
+        //    //return "This is the Welcome action method...";
+        //}
+        //public string Welcome(string name, int numTimes)
+        //{
+        //    return HtmlEncoder.Default.Encode($"hi, {name}, NumTimes is: {numTimes}");
+        //    //return "This is the Welcome action method...";
+        //}
+    }
+}
---
+++ MvcMovie/Migrations/20220824071125_letterUPD3.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    public partial class letterUPD3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+            name: "Titile",
+            table: "Movie",
+            newName: "Title",
+            schema: "dbo");
+
+            migrationBuilder.RenameColumn(
+            name: "ReleasedDate",
+            table: "Movie",
+            newName: "ReleaseDate",
+            schema: "dbo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           migrationBuilder.RenameColumn(
+           name: "Title",
+           table: "Movie",
+           newName: "Titile",
+           schema: "dbo");
+
+            migrationBuilder.RenameColumn(
+            name: "ReleaseDate",
+            table: "Movie",
+            newName: "ReleasedDate",
+            schema: "dbo");
+        }
+    }
+}
