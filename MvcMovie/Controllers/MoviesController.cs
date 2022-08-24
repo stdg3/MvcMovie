73322b896804aef92d61a18194388a119e1f84d5
stdg3 <velikoglosv@mail.ru>
2022-08-24 13:55:09 +0300

    added new row tosql<rate>, included serch method

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -20,11 +20,63 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index(string id)
+        //{
+        //    var movies = from m in _context.Movie
+        //                 select m;
+        //    if (!String.IsNullOrEmpty(id))
+        //    {
+        //        movies = movies.Where(s => s.Title!.Contains(id));
+        //    }
+        //    return View(await movies.ToListAsync());
+        //    //return _context.Movie != null ? 
+        //    //            View(await _context.Movie.ToListAsync()) :
+        //    //            Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        //}
+        //public async Task<IActionResult> Index(string searchString)
+        //{
+        //    var movies = from m in _context.Movie
+        //                 select m;
+        //    if (!String.IsNullOrEmpty(searchString))
+        //    {
+        //        movies = movies.Where(s => s.Title!.Contains(searchString));
+        //    }
+        //    return View(await movies.ToListAsync());
+        //    //return _context.Movie != null ?
+        //    //            View(await _context.Movie.ToListAsync()) :
+        //    //            Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        //}
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-              return _context.Movie != null ? 
-                          View(await _context.Movie.ToListAsync()) :
-                          Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            // use LINQ to get list of generes
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+            return View(movieGenreVM);
+            //return _context.Movie != null ?
+            //            View(await _context.Movie.ToListAsync()) :
+            //            Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        }
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
         }
 
         // GET: Movies/Details/5
@@ -56,7 +108,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +140,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
             {
